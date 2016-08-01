72aa7c874a44b5ac26f8e61b41350de2fc65bbe4
semiguy <chulbeom.cho@gmail.com>
2016-08-01 14:21:46 +0900

    [QT5]Add. QDesktopWidget Sample

---
+++ QDesktopWidget/QDesktopWidget.pro
@@ -0,0 +1,21 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2016-08-01T14:01:18
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = QDesktopWidget
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        widget.cpp
+
+HEADERS  += widget.h
+
+RESOURCES += \
+    resources.qrc
---
+++ QDesktopWidget/widget.h
@@ -0,0 +1,35 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QSlider>
+#include <QPixmap>
+
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = 0);
+    ~Widget();
+
+    QWidget *canvas;
+    QSlider *slider;
+    QPixmap pixmap;
+
+    void paintEvent(QPaintEvent *event);
+
+public slots:
+    void setZoom(int new_zoom);
+
+private:
+    qreal zoom;
+    qreal pixels;
+    qreal px_width;
+    qreal px_height;
+    qreal mm_width;
+    qreal mm_height;
+};
+
+#endif // WIDGET_H
